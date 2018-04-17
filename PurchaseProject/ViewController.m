7ed6bbd70739b6e4e39d355093c3237685f2174b
wangyun-hero <wangyun120411@163.com>
2018-04-17 16:06:57 +0800

    添加pch文件并进行配置

--- PurchaseProject/ViewController.m
+++ PurchaseProject/ViewController.m
@@ -16,8 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor greenColor];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor yellowColor];
+    
+    
 }
 
 
